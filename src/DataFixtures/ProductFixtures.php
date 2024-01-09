cc6f068a18b574ef6f58cb7b1f473c7aafe1378a
alleidda <aaddiella@gmail.com>
2024-01-10 00:29:52 +0100

    Adding product and product fixtures

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProductFixtures extends Fixture 
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $product = new Product;
+            $product->setName('Phone ' . $i);
+            $product->setDescription('Description phone ' . $i);
+            $product->setPrice(mt_rand(750, 1500));
+            $product->setQuantity(mt_rand(3, 8));
+            $product->setModel('Model phone ' . $i);
+
+            $manager->persist($product);
+        }
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datePublish = null;
+
+    public function __construct()
+    {
+        $this->setDatePublish(new \DateTimeImmutable());
+    }
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getDatePublish(): ?\DateTimeInterface
+    {
+        return $this->datePublish;
+    }
+
+    public function setDatePublish(\DateTimeInterface $datePublish): static
+    {
+        $this->datePublish = $datePublish;
+
+        return $this;
+    }
+}

4195115890539a2bf1dacc1f2518a3f7fbb52980
alleidda <aaddiella@gmail.com>
2024-01-11 03:27:22 +0100

    Adding documentation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,9 +20,35 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class UserController extends AbstractController
-{
+{   
+        /**
+     * @OA\Get(
+     *   tags={"Users"},
+     *   summary="Get all users owned by the current customer",
+     *   @OA\Response(response=200, description="All users owned by the current customer"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No user found"),
+     *   @OA\Parameter(
+     *     name="page",
+     *     description="Current page number",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer")
+     *   ),
+     *   @OA\Parameter(
+     *     name="limit",
+     *     description="Limit items per page",
+     *     in="query",
+     *     required=false,
+     *     @OA\Schema(type="integer")
+     *   )
+     * )
+     */
     #[Route('/api/users', name: 'users', methods: ['GET'])]
     public function getAllUser(Request $request,Pagination $paginator, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
@@ -49,7 +75,35 @@ class UserController extends AbstractController
        return new JsonResponse($jsonUsersList, Response::HTTP_OK, [], true);
     }
 
+    public function userNotExist(?User $user)
+    {
+        if (!$user) {
+            throw new NotFoundHttpException("No user found with this ID");
+        }
+    }
 
+    public function isNotOwner(string $attribute, User $user, string $message)
+    {
+        // If current customer is not the owner return an exception
+        if (!$this->isGranted($attribute, $user)) {
+            throw new HttpException(JsonResponse::HTTP_UNAUTHORIZED, $message);
+        }
+    }
+
+
+        /**
+     * @OA\Get(
+     *   tags={"Users"},
+     *   summary="Get a user by ID",
+     *   @OA\Response(response=200, description="User details"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No user found with this ID"),
+     *   @OA\PathParameter(
+     *     name="id",
+     *     description="ID of the user you want to recover"
+     *   )
+     * )
+     */
     #[Route('/api/users/{id}', name: 'user', methods: ['GET'])]
     public function getUserbyId(User $user, SerializerInterface $serializer): JsonResponse
     {
@@ -64,14 +118,38 @@ class UserController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
+    /**
+     * @OA\Post(
+     *   tags={"Users"},
+     *   summary="Create a new user",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         type="object",
+     *       )
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=201,
+     *     description="Created user",
+     *     @OA\JsonContent(
+     *       type="array",
+     *       @OA\Items(ref=@Model(type=User::class, groups={"getUsers"}))
+     *     )
+     *   ),
+     *   @OA\Response(response=400, description="JSON field validation failed"),
+     *   @OA\Response(
+     *     response=401,
+     *     description="JWT unauthorized error"
+     *   ),
+     *   @OA\Response(response=500, description="JSON syntax error or no JSON sent in the request body"),
+     * )
+     */
+
     #[Route('/api/users', name: 'addUser', methods: ['POST'])]
-    public function createUser(
-    Request $request,
-    EntityManagerInterface $em,
-    SerializerInterface $serializer,
-    UrlGeneratorInterface $urlGenerator,
-    ValidatorInterface $validator
-    ): JsonResponse
+    public function createUser(Request $request,EntityManagerInterface $em,SerializerInterface $serializer,UrlGeneratorInterface $urlGenerator,ValidatorInterface $validator): JsonResponse
     {
         $data = $request->getContent();
         $user = $serializer->deserialize($data, User::class, 'json');
@@ -93,21 +171,19 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["location" => $location], true);
     }
 
-    public function userNotExist(?User $user)
-    {
-        if (!$user) {
-            throw new NotFoundHttpException("No user found with this ID");
-        }
-    }
-
-    public function isNotOwner(string $attribute, User $user, string $message)
-    {
-        // If current customer is not the owner return an exception
-        if (!$this->isGranted($attribute, $user)) {
-            throw new HttpException(JsonResponse::HTTP_UNAUTHORIZED, $message);
-        }
-    }
-
+    /**
+     * @OA\Delete(
+     *   tags={"Users"},
+     *   summary="Delete a user by ID",
+     *   @OA\Response(response=204, description="User successfully deleted"),
+     *   @OA\Response(response=401, description="JWT unauthorized error"),
+     *   @OA\Response(response=404, description="No user found with this ID"),
+     *   @OA\PathParameter(
+     *     name="id",
+     *     description="ID of the user you want to delete"
+     *   )
+     * )
+     */
     #[Route('/api/users/{id}', name: 'deleteUser', methods: ['DELETE'])]
     public function deleteUser(?User $user, EntityManagerInterface $em, TagAwareCacheInterface $cachePool): JsonResponse
     {
